eb3196fc09df331f883aab53d8d5fcae25391b31
danielcr <daniel.correa.1995@gmail.com>
2022-06-07 10:17:49 -0500

    submit funciona

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ window.addEventListener('DOMContentLoaded', function(){
     let formRegister = document.querySelector(".form-register");
     formRegister.addEventListener("submit", function(e){
         // e.preventDefault();
-        let canSubmit = false;
+        var canSubmit = true;
+
         let inputName = document.querySelector("#fName");
 
         if(inputName.value == ""){
             document.querySelector(".input-container.fName .input-error").innerHTML = "El nombre es obligatorio"
-            let canSubmit = true;
+            canSubmit = false;
 
         }else{
             if (inputName.value.length<3){
                 document.querySelector(".input-container.fName .input-error").innerHTML = "El nombre tiene que tener minimo 3 caracteres"
-            let canSubmit = true;
+            canSubmit = false;
 
             }else{
                 document.querySelector(".input-container.fName .input-error").innerHTML = ""
@@ -23,12 +24,12 @@ window.addEventListener('DOMContentLoaded', function(){
 
         if(inputLName.value == ""){
             document.querySelector(".input-container.lName .input-error").innerHTML = "El apellido es obligatorio"
-            let canSubmit = true;
+            canSubmit = false;
 
         }else{
             if (inputLName.value.length<3){
                 document.querySelector(".input-container.lName .input-error").innerHTML = "El apellido tiene que tener minimo 3 caracteres"
-                let canSubmit = true;
+                canSubmit = false;
 
             }else{
                 document.querySelector(".input-container.lName .input-error").innerHTML = ""
@@ -39,12 +40,12 @@ window.addEventListener('DOMContentLoaded', function(){
         // console.log(inputCedula.value)
         if(inputCedula.value == ""){
             document.querySelector(".input-container.cedula .input-error").innerHTML = "La cedula es obligatoria"
-            let canSubmit = true;
+            canSubmit = false;
 
         }else{
-            if (inputCedula.value.length < 10 || inputCedula.value.length > 10 || isNaN(Number(inputCedula.value))){
+            if (inputCedula.value.length != 10 || isNaN(Number(inputCedula.value)) || Number(inputCedula.value)<=0){
                 document.querySelector(".input-container.cedula .input-error").innerHTML = "La cedula tiene que tener 10 caracteres y ser solo numeros"
-                let canSubmit = true;
+                canSubmit = false;
             }else{
                 document.querySelector(".input-container.cedula .input-error").innerHTML = ""
             }            
@@ -55,21 +56,21 @@ window.addEventListener('DOMContentLoaded', function(){
 
         if(inputEmail.value == ""){
             document.querySelector(".input-container.email .input-error").innerHTML = "El email es obligatorio"
-            let canSubmit = true;
+            canSubmit = false;
 
         }else{
             let index1 = inputEmail.value.indexOf("@");
             let index2 = inputEmail.value.indexOf(".");
             if (index1<0 || index2<0 || index1 > index2){
                 document.querySelector(".input-container.email .input-error").innerHTML = "El email es invalido"
-                let canSubmit = true;
+                canSubmit = false;
 
             }else{
                 document.querySelector(".input-container.email .input-error").innerHTML = ""
             }            
         }
 
-        if(canSubmit){
+        if(!canSubmit){
             e.preventDefault()
         }
     })
